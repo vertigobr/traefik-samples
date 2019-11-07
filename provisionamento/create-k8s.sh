b8df17e33dc49042a0429d9c150c4af80327fba3
Andre Fernandes <andre@vertigo.com.br>
2019-11-07 07:56:03 -0300

    k8s create cluster script

---
+++ provisionamento/create-k8s.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+echo "Lista de clusters:"
+doctl kubernetes cluster list
+echo "Criando novo cluster:"
+doctl kubernetes cluster create meetup --region nyc3 --node-pool "name=mainpool;size=s-2vcpu-4gb;count=2"
+echo "Exportando kubeconfig:"
+doctl kubernetes cluster kubeconfig save meetup
+
